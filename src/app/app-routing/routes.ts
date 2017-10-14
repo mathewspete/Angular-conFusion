caf81d3b9540f4c2d463a12cfd7d9c982d545f01
mathews225 <petemathews14@gmail.com>
2017-10-14 15:09:59 -0400

    Single Page Applications Part 2

--- src/app/app-routing/routes.ts
+++ src/app/app-routing/routes.ts
@@ -7,8 +7,9 @@ import { AboutComponent } from '../about/about.component';
 import { ContactComponent } from '../contact/contact.component';
 
 export const routes: Routes = [
-  { path: 'home',  component: HomeComponent },
-  { path: 'menu',     component: MenuComponent },
+  { path: 'home', component: HomeComponent },
+  { path: 'menu', component: MenuComponent },
+  { path: 'dishdetail/:id', component: DishdetailComponent },
   { path: '', redirectTo: '/home', pathMatch: 'full' },
-  { path: 'contactus',     component: ContactComponent }
+  { path: 'contactus', component: ContactComponent }
 ];
